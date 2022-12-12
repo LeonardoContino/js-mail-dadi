2d3075309b127f223951616563f11d23709d67ee
LeonardoContino <leoconti2000@hotmail.it>
2022-12-12 14:27:04 +0100

    commenti aggiunti

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("js ok");
+// console.log("js ok");
+/*
+ESERCIZIO 1 - Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampiamo in pagina i due tiri e il risultato
+-----------------------------------------------------------------
+/*
+1.prendere l'elemento nel dom
+2.generare un numero random con math random sia per il cpu sia per user
+3. validare con if
+4.stampare con inner.html
+*/
